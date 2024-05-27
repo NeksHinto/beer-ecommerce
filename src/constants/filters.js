ad7425157b7702781b3a8fcbf490cea3509fa8f7
NeksHinto <nhinojo@itba.edu.ar>
2024-05-27 11:34:52 -0400

    Constants

---
+++ src/constants/filters.js
@@ -0,0 +1,10 @@
+const beerIcon = "/resources/icons/Beer.png";
+const wineIcon = "/resources/icons/Wine-glass.png";
+
+const filters = [
+  { text: "All", value: "", icon: "" },
+  { text: "Beer", value: "beer", icon: beerIcon },
+  { text: "Wine", value: "wine", icon: wineIcon },
+];
+
+export default filters;
