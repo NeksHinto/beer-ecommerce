63653abaa16917046d89e0374089c6e4b7f25d7b
NeksHinto <nhinojo@itba.edu.ar>
2024-05-27 11:33:01 -0400

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DetailPage from "./pages/Detail";
+import HomePage from "./pages/Home";
+import theme from "./styles/theme";
+import { CssBaseline } from "@mui/material/";
+import { AppContextProvider } from "./contexts/app";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <AppContextProvider>
+        <CssBaseline />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/:product" element={<DetailPage />} />
+          </Routes>
+        </Router>
+      </AppContextProvider>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
